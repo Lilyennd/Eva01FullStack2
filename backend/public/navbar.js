f465457fdadaf12990239f2d02298d5019f6d934
Lilyennd <victoriariffo21@gmail.com>
2026-09-03 23:51:02 -0400

    Creacion de nuevo producto y arreglo de navbar con botones de cerrar sesion

--- backend/public/navbar.js
+++ backend/public/navbar.js
@@ -1,18 +1,24 @@
 const usuario = JSON.parse(localStorage.getItem('usuarioActual'));
 
-if (usuario && (usuario.id_rol === 1 || usuario.id_rol === 2)) {
-  document.getElementById('menuAdmin').style.display = 'flex';
-} else {
-
-  document.getElementById('menuComprador').style.display = 'flex';
-}
-
+const menuAdmin = document.getElementById('menuAdmin');
+const menuComprador = document.getElementById('menuComprador');
+const btnIniciarSesion = document.getElementById('btnIniciarSesion');
+const btnCerrarSesion = document.getElementById('btnCerrarSesion');
 
 if (usuario) {
-  document.getElementById('btnCerrarSesion').style.display = 'inline-block';
-}
+  if (btnIniciarSesion) btnIniciarSesion.style.display = 'none';
+  if (btnCerrarSesion) btnCerrarSesion.style.display = 'inline-block';
 
-document.getElementById('btnCerrarSesion').addEventListener('click', () => {
-  localStorage.removeItem('usuarioActual'); 
-  window.location.href = 'login.html';      
-});
+  if (usuario.id_rol === 1 || usuario.id_rol === 2) {
+    if (menuComprador) menuComprador.style.display = 'none';
+    if (menuAdmin) menuAdmin.style.display = 'flex';
+  } else {
+    if (menuComprador) menuComprador.style.display = 'flex';
+  }
+} 
+if (btnCerrarSesion) {
+  btnCerrarSesion.addEventListener('click', () => {
+    localStorage.removeItem('usuarioActual'); 
+    window.location.href = 'home.html'; 
+  });
+}
